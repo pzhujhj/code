e1e96529a2d730d4007bdefe3436e90c788743ce
jihanjun@logread.com <jihanjun@logread.com>
2017-07-05 10:36:42 +0800

    增加注释

--- va_List/system_cmd.c
+++ va_List/system_cmd.c
@@ -18,6 +18,7 @@ static int system_cmd(char *fmt, ...)
 		va_start(ap, fmt);
 		vsnprintf(p, 256, fmt, ap);
 		va_end(ap);
+		//下面的execlp方式是执行复杂的shell命令，通常情况下这样使用
 		execlp("/bin/sh", "/bin/sh", "-c", p, NULL);
 		syslog(0, "oooppps, called %s which does not exist\n", p);
 		exit(0);
@@ -38,6 +39,7 @@ static int system_cmd(char *fmt, ...)
 	va_start(ap, fmt);
 	vsnprintf(p, 256, fmt, ap);
 	va_end(ap);
+	//执行成功返回0
 	ret = system(p);
 	return ret;
 }
@@ -46,7 +48,7 @@ static int system_cmd(char *fmt, ...)
 int main()
 {
 	//system_cmd("echo %d %s>> /test/1.txt", 2, "cccc");	
-	system_cmd("cat /test/1.txt");	
+	system_cmd("ls -lh system_cmd.c");
 	
 	return 0;
 }
