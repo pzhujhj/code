8d5067b4616ce5a1216e2f9fdbfb40489af003ee
jihj <pzhujhj@126.com>
2017-10-11 22:05:42 +0800

    提交冒泡排序法

---
+++ Sort/bubble_sort.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+
+/************************************************************************************************
+*基本思想： 不断比较相邻的两个数，让较大的数不断地往后移。经过一番比较，就选出了最大的数。
+*经过第二轮比较，就选出了次大的数。以此类推。 那么对于大小为N的数组，需要N-1轮比较。
+*************************************************************************************************/
+/*使用宏的方式比较好，可以体会一下*/
+#define key(A) (A)
+#define less(A, B) (key(A) < key(B)) // 宏定义一个大小判断的宏
+#define exch(A, B) {int t = A; A = B; B = t;}// 交换两个数值大小的宏
+#define compexch(A, B) if(less(B, A)) exch(A, B)// 判断大小，并进行交换的宏，如果A大于B，两者进行交换
+
+static void bubble_sort(int buf[], int len)
+{
+	int i, j;
+
+	for (i = 0; i < len; i++) {
+		for (j = i+1; j < len; j++) {
+			if (less(buf[j], buf[i]))
+				compexch(buf[i], buf[j]);
+		}
+		
+	}
+}
+
+int main()
+{
+	int length, i;
+	int array[] = {12 ,47 ,478 ,56 ,98 ,33 ,212 ,24, 165, 98};
+
+	length = sizeof(array)/sizeof(int);
+
+	for(i = 0; i < length; i++) {
+		printf("%d ", array[i]);
+	}
+
+	printf("\n");
+
+	bubble_sort(array, length);
+	
+	for(i = 0; i < length; i++) {
+		printf("%d ", array[i]);
+	}
+
+	printf("\n");
+	return 0;
+}
--- Sort/select_sort.c
+++ Sort/select_sort.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 /************************************************************************************************
-*ѡ������:��һ���������ҵ���С��Ԫ��Ȼ����͵�һ��Ԫ�ؽ���������ʵ�ֵ�һ��Ԫ������С�ģ�
-*Ȼ��ڶ��ξ����ҳ���С��Ԫ�غ͵ڶ���Ԫ�ؽ��������ν��У�ֱ�����������һ��Ԫ�أ���������������
+*选择排序法:从一个数组中找到最小的元素然后将其和第一个元素交换，这样实现第一个元素是最小的，
+*然后第二次就是找出次小的元素和第二个元素交换，依次进行，直到交换到最后一个元素，整个排序过程完成
 *************************************************************************************************/
 
 #if 0
@@ -12,37 +12,37 @@ static void choose_select(int buf[], int len)
 	int tmp = 0;
 
 	for (i = 0; i < len; i++) {
-		min = i;//���Ȱ�����ĵ�һ����ֵ��Ϊ����С��
+		min = i;//首先把数组的第一个数值认为是最小的
 		for (j = i+1; j < len; j++) {
 			if (buf[j] < buf[min])
-				min = j; //���a[j]С��a[min],���±긳ֵΪj
+				min = j; //如果a[j]小于a[min],则下标赋值为j
 		}
 
-		//����С��ֵ���н���
+		//将最小的值进行交换
 		tmp = buf[i];
 		buf[i] = buf[min];
 		buf[min] = tmp;
 	}
 }
 #else
-/*ʹ�ú�ķ�ʽ�ȽϺã��������һ��*/
+/*使用宏的方式比较好，可以体会一下*/
 #define key(A) (A)
-#define less(A, B) (key(A) < key(B)) // �궨��һ����С�жϵĺ�
-#define exch(A, B) {int t = A; A = B; B = t;}// ����������ֵ��С�ĺ�
-#define compexch(A, B) if(less(B, A)) exch(A, B)// �жϴ�С�������н����ĺ꣬���A����B�����߽��н���
+#define less(A, B) (key(A) < key(B)) // 宏定义一个大小判断的宏
+#define exch(A, B) {int t = A; A = B; B = t;}// 交换两个数值大小的宏
+#define compexch(A, B) if(less(B, A)) exch(A, B)// 判断大小，并进行交换的宏，如果A大于B，两者进行交换
 
 static void choose_select(int buf[], int len)
 {
 	int i, j, min;
 
 	for (i = 0; i < len; i++) {
-		min = i;//���Ȱ�����ĵ�һ����ֵ��Ϊ����С��
+		min = i;//首先把数组的第一个数值认为是最小的
 		for (j = i+1; j < len; j++) {
 			if (less(buf[j], buf[min]))
-				min = j; //���a[j]С��a[min],���±긳ֵΪj
+				min = j; //如果a[j]小于a[min],则下标赋值为j
 		}
 
-		//����С��ֵ���н���
+		//将最小的值进行交换
 		compexch(buf[i], buf[min]);
 	}
 }
